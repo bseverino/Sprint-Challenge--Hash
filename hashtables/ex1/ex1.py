a70b120512ac895b938542524f583cb529e35048
B Severino <biancasev@gmail.com>
2020-05-08 11:22:12 -0400

    Completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,13 @@
 def get_indices_of_item_weights(weights, length, limit):
+    cache = {}
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(weights)):
+        if limit - weights[i] in cache:
+            print([i, cache[limit - weights[i]]])
+            return [i, cache[limit - weights[i]]]
+        if weights[i] not in cache:
+            cache[weights[i]] = i
 
     return None
+
+print(get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21))
