7790a6329f662a72b50714ee88fe1b715c7fd008
B Severino <biancasev@gmail.com>
2020-05-08 11:59:13 -0400

    Completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,19 @@
 def has_negatives(a):
+    cache = {}
+    result = []
 
-    """
-    YOUR CODE HERE
-    """
+    for i in a:
+        if i not in cache:
+            if i > 0:
+                cache[f'positive {i}'] = i
+            elif i < 0:
+                cache[f'negative {-i}'] = i
+        if i > 0:
+            if f'negative {i}' in cache:
+                result.append(i)
+        if i < 0:
+            if f'positive {-i}' in cache:
+                result.append(cache[f'positive {-i}'])
 
     return result
 
